73d5fcfd572d18c76596e736af5e729745520dd7
AhmedSa-mir <asamir97@hotmail.com>
2018-03-15 14:54:56 +0200

    Fixed HTML encoding, Fixed tail coloring while moving  on the grid

--- player.js
+++ player.js
@@ -1,120 +1,128 @@
-
-class Dir{
-
-	constructor(x,y){
-		this.x = x;
-		this.y = y;	
-	}
-	equal(f){
-		console.log(f.y-this.y);
-		
-		if(this.x != f.x || this.y != f.y)
-			return false;
-		return true;
-	}
-
-
-}
-class Position{
-	constructor(x,y){
-		this.x =x;
-		this.y =y;
-	}
-
-}
-class Player{
-
-constructor(dir,position,id){
-this.dir = dir;
-this.last_dir = new Dir(dir.x,dir.y);
-this.position = position;
-this.area = 0;
-this.name = "samir";
-this.ID = id;
-
-}
-}
-
-function HandleMovement(){
-if (keyIsDown(RIGHT_ARROW)) {
-player.dir.x = 1;
-player.dir.y = 0;
-}
-
-if (keyIsDown(LEFT_ARROW)) {
-player.dir.x = -1;
-player.dir.y = 0;
-}
-
-if (keyIsDown(UP_ARROW)) {
-player.dir.x = 0;
-player.dir.y = -1;
-}
-
-if (keyIsDown(DOWN_ARROW)) {
-player.dir.x = 0;
-player.dir.y = 1;
-}
-
-player.position.x +=player.dir.x * speed;
-player.position.y += player.dir.y * speed;
-	
+class Dir {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    equal(f) {
+        if (this.x != f.x || this.y != f.y)
+            return false;
+        return true;
+    }
 }
 
 
-
-function drawPlayers(){
-	for(i = 0; i<=number_of_blocks_width; i++)
-{
-for( j = 0; j<=number_of_blocks_height; j++)
-if(grid[i][j] !=0)
-{	
-	noStroke();
-	fill(color(COLORS[grid[i][j]]));
-
-	rect(i*block_size
-		,j*block_size
-		,block_size
-		,block_size);
-}
-}
-
-//player color
-noStroke();
-fill(color(COLORS[player.ID]));
-let x = player.position.x / block_size;
-let y = player.position.y / block_size;
-rect(x*block_size,y*block_size,block_size,block_size);
-
-
-
-}
-
-function updateGrid(){
-let x = Math.round(player.position.x / block_size);
-let y = Math.round(player.position.y / block_size);
-//tail color
-grid[x][y] = player.ID+1;
-
-
-}
-function fixDir(){
-	//console.log(player.dir.y==player.last_dir.y)
-if(!player.dir.equal(player.last_dir))
-{
-	console.log("not ");
-
-	player.position.x = Math.round(player.position.x / block_size) * block_size;
-	player.position.y = Math.round(player.position.y / block_size)* block_size;
-	
-	
-}
-	
+class Position {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 
-function finalize(){
-player.last_dir.x = player.dir.x;
-player.last_dir.y = player.dir.y;
+class Player {
 
+    constructor(dir, position, id) {
+        this.dir = dir;
+        this.last_dir = new Dir(dir.x, dir.y);
+        this.position = position;
+        this.area = 0;
+        this.name = "samir";
+        this.ID = id;
+    }
+}
+
+
+function handleMovement() {
+    if (keyIsDown(RIGHT_ARROW)) {
+        player.dir.x = 1;
+        player.dir.y = 0;
+    }
+
+    if (keyIsDown(LEFT_ARROW)) {
+        player.dir.x = -1;
+        player.dir.y = 0;
+    }
+
+    if (keyIsDown(UP_ARROW)) {
+        player.dir.x = 0;
+        player.dir.y = -1;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+        player.dir.x = 0;
+        player.dir.y = 1;
+    }
+
+    player.position.x += player.dir.x * speed;
+    player.position.y += player.dir.y * speed;
+}
+
+
+function drawGrid() {
+
+    for (i = 0; i <= number_of_blocks_width; ++i) {
+        for (j = 0; j <= number_of_blocks_height; ++j) {
+            if (grid[i][j] == 2) {
+                noStroke();
+                fill(color(COLORS[grid[i][j]]));
+                rect(i * block_size, j * block_size, block_size + 1, block_size + 1); // +1 for filling gaps between cells 
+            }
+        }
+    }
+
+    // Player color
+    fill(color(COLORS[player.ID]));
+
+    let x = player.position.x / block_size;
+    let y = player.position.y / block_size;
+    rect(x * block_size, y * block_size, block_size + 1, block_size + 1); // +1 for filling gaps between cells
+}
+
+
+function updateGrid() {
+
+    let x = 0;
+    let y = 0;
+
+    // Player moving down 
+    if (player.dir.equal(new Dir(0, 1))) {
+        x = Math.round(player.position.x / block_size);
+        y = Math.floor(player.position.y / block_size);
+    }
+
+    // Player moving up
+    if (player.dir.equal(new Dir(0, -1))) {
+        x = Math.round(player.position.x / block_size);
+        y = Math.ceil(player.position.y / block_size);
+    }
+
+    // Player moving left 
+    if (player.dir.equal(new Dir(1, 0))) {
+        x = Math.floor(player.position.x / block_size);
+        y = Math.round(player.position.y / block_size);
+    }
+
+    // Player moving right
+    if (player.dir.equal(new Dir(-1, 0))) {
+        x = Math.ceil(player.position.x / block_size);
+        y = Math.round(player.position.y / block_size);
+    }
+
+    // Tail color
+    grid[x][y] = player.ID + 1;
+}
+
+
+function fixDir() {
+
+    if (!player.dir.equal(player.last_dir)) {
+        player.position.x = Math.round(player.position.x / block_size) * block_size;
+        player.position.y = Math.round(player.position.y / block_size) * block_size;
+    }
+}
+
+
+function finalize() {
+    player.last_dir.x = player.dir.x;
+    player.last_dir.y = player.dir.y;
 }
--- sketch.js
+++ sketch.js
@@ -1,58 +1,56 @@
-var x = 0;
 var KEY_PRESSED = false;
-var COLORS=['#990000','#ff6666'];
-var grid =[];
-var inner_width;
-var inner_height;
-var aspect_ratio = 16/9; 	
-var view_blocks_number =30;
+var COLORS = ['reserved', '#990000', '#ff6666']; // Player1, Player1_Tail, Player2, Player2_Tail, ...
+var grid = [];
+var aspect_ratio;
+var view_blocks_number = 30;
 var block_width;
 var block_height;
 var number_of_blocks_height;
 var number_of_blocks_width;
-var player ;
+var player;
 var speed = 3;
 var block_size;
+const canvas_length = 150;
+//const grid_length = 100;
+const border_width_cells = 2;
+
+function initGrid() {
+    for (i = 0; i < canvas_length; ++i) {
+        grid[i] = [];
+        for (j = 0; j < canvas_length; ++j) {
+            grid[i][j] = 0;
+        }
+    }
+}
+
 function setup() {
 
-	player = new Player(new Dir(0,0),new Position(0,0),0);
-	
+    player = new Player(new Dir(0, 0), new Position(0, 0), 1);
 
-	aspect_ratio = windowWidth / windowHeight;
+    aspect_ratio = windowWidth / windowHeight;
 
-	block_size=view_blocks_number * (Math.max(windowWidth,windowHeight)/1000);
-	number_of_blocks_height=Math.round(windowHeight/block_size);
-	number_of_blocks_width=Math.round(windowWidth/block_size);
-	console.log(block_size)
-	console.log(number_of_blocks_height)
-	
-	createCanvas(windowWidth,windowHeight);
-//console.log((Math.max(block_width,block_height)/100));
-//console.log(windowHeight);
+    block_size = view_blocks_number * (Math.max(windowWidth, windowHeight) / 1000);
+    number_of_blocks_height = Math.ceil(windowHeight / block_size);
+    number_of_blocks_width = Math.ceil(windowWidth / block_size);
 
-for(i = 0; i<100; i++)
-{
-	grid[i]=[];
-for( j = 0; j<100; j++)
-{
-grid[i][j] = 0;
-}
-}
-//createCanvas(,1);
-  background(200);
-///  line(15, 25, 70, 90);
+    createCanvas(windowWidth, windowHeight);
+
+    initGrid();
 }
 
 function draw() {
-//clear screen
- background(255);
-//change positions;
-HandleMovement();
-fixDir();
 
-updateGrid();
-//draw players
- drawPlayers();
- finalize();
+    // Clear screen
+    background(255);
 
+    // Change players positions;
+    handleMovement();
+
+    fixDir();
+
+    updateGrid();
+
+    drawGrid();
+
+    finalize();
 }
