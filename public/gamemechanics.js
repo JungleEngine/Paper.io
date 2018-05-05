ad02b2e7c0b8323044d5e9a035f2757502c50abf
3bhady <mohamed_abdelhady96@hotmail.com>
2018-05-05 04:57:50 +0200

    --

--- public/gamemechanics.js
+++ public/gamemechanics.js
@@ -65,8 +65,8 @@ function simulate() {
                 }
             }
             let head = {};
-            head.x = tailPos.x + (0.5 * player.dir_x);
-            head.y = tailPos.y + (0.5 * player.dir_y);
+            head.x = tailPos.x + (0.5 * player.dir.x);
+            head.y = tailPos.y + (0.5 * player.dir.y);
             if ( (player.fix_pos_x-Math.round(head.x))*player.dir.x < 0 ||  (player.fix_pos_y- Math.round(head.y))*player.dir.y < 0) {
                 console.log("The rare case has happened");
                 return ;
